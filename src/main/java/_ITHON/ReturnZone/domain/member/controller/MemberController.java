9ce0dbfc462fdd11303706ae75575fbd0c103720
d6nggyun <d6nggyun@gmail.com>
2025-06-29 22:54:42 +0900

    docs: 회원 스웨거

--- src/main/java/_ITHON/ReturnZone/domain/member/controller/MemberController.java
+++ src/main/java/_ITHON/ReturnZone/domain/member/controller/MemberController.java
@@ -3,6 +3,11 @@ package _ITHON.ReturnZone.domain.member.controller;
 import _ITHON.ReturnZone.domain.member.dto.req.SignupRequestDto;
 import _ITHON.ReturnZone.domain.member.dto.res.SignupResponseDto;
 import _ITHON.ReturnZone.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,11 +17,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
+@Tag(name = "Member", description = "회원 관련 API")
 public class MemberController {
 
     private final MemberService memberService;
 
-    // 회원가입
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다. (비밀번호는 최소 8자 이상이어야 합니다.)",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "회원가입 성공",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = SignupResponseDto.class))
+                    )
+            }
+    )
     @PostMapping("/signup")
     public ResponseEntity<SignupResponseDto> signup(@Valid @RequestBody SignupRequestDto signupRequestDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.signup(signupRequestDto));
