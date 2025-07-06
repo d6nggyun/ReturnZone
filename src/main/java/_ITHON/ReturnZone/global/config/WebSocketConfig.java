b7dacc63efad84579285df8077a0e93a5a0d3262
d6nggyun <d6nggyun@gmail.com>
2025-07-07 02:59:12 +0900

    feat: 채팅

--- src/main/java/_ITHON/ReturnZone/global/config/WebSocketConfig.java
+++ src/main/java/_ITHON/ReturnZone/global/config/WebSocketConfig.java
@@ -14,13 +14,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     // 메시지 브로커 경로 규칙
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 서버 → 클라이언트 브로커
         registry.enableSimpleBroker("/topic", "/queue");
+        // 클라이언트 → 서버 전송 prefix
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
