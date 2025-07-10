8ea30a29c2d666677285569d8aa7ced188226b2e
d6nggyun <d6nggyun@gmail.com>
2025-07-10 19:03:18 +0900

    fix: Bank_Account Id 생성 전략 수정

--- src/main/java/_ITHON/ReturnZone/domain/member/entity/BankAccount.java
+++ src/main/java/_ITHON/ReturnZone/domain/member/entity/BankAccount.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class BankAccount {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "member_id", nullable = false)
