a9a6ac31568c99d9a157b8f4b82b492364b6924e
d6nggyun <d6nggyun@gmail.com>
2025-07-12 08:13:30 +0900

    docs: 스웨거 jwt 적용

--- src/main/java/_ITHON/ReturnZone/global/config/SwaggerConfig.java
+++ src/main/java/_ITHON/ReturnZone/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package _ITHON.ReturnZone.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,9 +32,22 @@ public class SwaggerConfig {
                 .url("http://localhost:8080")
                 .description("로컬 개발 서버");
 
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // 보안 요구 사항 (전역 적용)
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
         // OpenAPI 객체 구성
         return new OpenAPI()
                 .info(info)
-                .servers(List.of(server, localServer));
+                .servers(List.of(server, localServer))
+                .components(new Components().addSecuritySchemes("bearerAuth", bearerAuth))
+                .addSecurityItem(securityRequirement);
     }
 }
